2fe28d2dcdfa4616413d38cea17ae4411a09dc64
HiroshiAdauto <hiroshi.tomioka95@gmail.com>
2024-08-31 17:05:52 -0300

    First part completed - backend

---
+++ backend/models/message.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose"
+
+const messageSchema = new mongoose.Schema({
+    senderId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User", //referencia al User model
+        required: true
+    },
+    receiverId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User", //referencia al User model
+        required: true
+    },
+    message: {
+        type: String,
+        required: true
+    }
+}, {timestamps: true})
+
+const Message = mongoose.model("Message", messageSchema)
+
+export default Message
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 import express from "express"
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
 
 import authRoutes from "./routes/auth.routes.js"
+import messageRoutes from "./routes/message.routes.js"
+import userRoutes from "./routes/user.routes.js"
+
 import connectToMongoDB from "./db/connectToMongoDB.js"
 
 const app = express()
@@ -10,8 +14,11 @@ const PORT = process.env.PORT || 5000
 dotenv.config()
 
 app.use(express.json()) // to parse the incoming requests with JSON payloads (from req.body)
+app.use(cookieParser())
 
 app.use("/api/auth", authRoutes)
+app.use("/api/messages", messageRoutes)
+app.use("/api/users", userRoutes)
 
 app.get("/", (req, res) => {
     res.send("Hello world")
