d3f5e031ddef4ae68a433c483bcaa21b87035ce6
Hiroshi Adauto <hiroshi.tomioka95@gmail.com>
2024-09-30 16:44:42 -0300

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
+import path from "path"
 
 import authRoutes from "./routes/auth.routes.js"
 import messageRoutes from "./routes/message.routes.js"
@@ -12,6 +13,8 @@ import { app, server } from "./socket/socket.js"
 
 const PORT = process.env.PORT || 5000
 
+const __dirname = path.resolve()
+
 dotenv.config()
 
 app.use(express.json()) // to parse the incoming requests with JSON payloads (from req.body)
@@ -21,8 +24,10 @@ app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 app.use("/api/users", userRoutes)
 
-app.get("/", (req, res) => {
-    res.send("Hello world")
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
 })
 
 server.listen(PORT, () => {
